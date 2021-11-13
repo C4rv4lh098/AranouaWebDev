a1814c8c2506f31cd7b56eb8de913a0bbf03e33e
C4rv4lh098 <rodrigo.carvalho.1998@gmail.com>
2021-11-13 10:54:41 -0400

    Autorização do Usuário parte2.1

--- backend/src/main/java/edu/ifam/aranoua/security/filter/JwtAuthorizationFilter.java
+++ backend/src/main/java/edu/ifam/aranoua/security/filter/JwtAuthorizationFilter.java
@@ -29,7 +29,7 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
             throws IOException, ServletException{
         String header = request.getHeader("Authorization");
 
-        if ((header != null && header.startsWith("Bearer ")){
+        if ((header != null && header.startsWith("Bearer "))){
             UsernamePasswordAuthenticationToken auth = getAuthentication(request, header.substring(7));
             if (auth != null){
                 SecurityContextHolder.getContext().setAuthentication(auth);
