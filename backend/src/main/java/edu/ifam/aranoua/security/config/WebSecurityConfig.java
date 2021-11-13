5d0882318e06d697b6318e02cd37ffc0c883c1b1
C4rv4lh098 <rodrigo.carvalho.1998@gmail.com>
2021-11-13 10:09:01 -0400

    Autenticação de Usuário

--- backend/src/main/java/edu/ifam/aranoua/security/config/WebSecurityConfig.java
+++ backend/src/main/java/edu/ifam/aranoua/security/config/WebSecurityConfig.java
@@ -1,13 +1,17 @@
 package edu.ifam.aranoua.security.config;
 
+import edu.ifam.aranoua.security.filter.JwtAuthenticationFilter;
+import edu.ifam.aranoua.security.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Arrays;
@@ -18,6 +22,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private Environment env;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     private static final String[] PUBLIC_MATCHERS = {
             "/h2-console/**",
@@ -39,6 +49,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
+        http.addFilter(new JwtAuthenticationFilter(authenticationManager(), jwtUtil));
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 
     @Bean
---
+++ backend/src/main/java/edu/ifam/aranoua/security/domain/Credencial.java
@@ -0,0 +1,26 @@
+package edu.ifam.aranoua.security.domain;
+
+import java.io.Serializable;
+
+public class Credencial implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String cpf;
+    private String senha;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
